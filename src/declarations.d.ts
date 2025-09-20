53162f7995258f4600efe0fc54da264bb9a843bf
unknown <valen1984@gmail.com>
2025-09-20 19:52:23 -0300

    update declarations.d.ts

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,17 @@ declare module "*.png" {
   const value: string;
   export default value;
 }
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_EMAILJS_SERVICE_ID: string;
+  readonly VITE_EMAILJS_TEMPLATE_ID: string;
+  readonly VITE_EMAILJS_PUBLIC_KEY: string;
+  readonly MERCADOPAGO_ACCESS_TOKEN: string;
+  readonly BACKEND_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
