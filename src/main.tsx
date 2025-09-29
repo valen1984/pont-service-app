12d1e74d4e7169a1ac9fa0f8c9b737d0bad31420
unknown <valen1984@gmail.com>
2025-09-28 23:39:05 -0300

    fix: imports con .js y rebuild

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
+// main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initMercadoPago } from "@mercadopago/sdk-react";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+// ⚡ Inicializar MercadoPago con tu PUBLIC_KEY
+initMercadoPago(import.meta.env.VITE_MERCADOPAGO_PUBLIC_KEY!, {
+  locale: "es-AR", // 👈 ajustá el locale si querés
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
