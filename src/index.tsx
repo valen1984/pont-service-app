6d44536e8a9cbf1a1d6ce7a7ef353feba0fc05d6
unknown <valen1984@gmail.com>
2025-09-29 01:46:41 -0300

    Final changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { initMercadoPago } from "@mercadopago/sdk-react";
-
-// ⚡ Inicializar Mercado Pago una sola vez
-const publicKey = import.meta.env.VITE_MERCADOPAGO_PUBLIC_KEY;
-if (publicKey) {
-  initMercadoPago(publicKey, { locale: "es-AR" });
-} else {
-  console.error("⚠️ Mercado Pago PUBLIC KEY no definida en .env");
-}
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
